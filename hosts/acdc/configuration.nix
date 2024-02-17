92fcbd72ba0bd0c5ade64ef8a80212630cd017e4
Robert <deangelis.robert13@gmail.com>
2024-02-17 22:35:04 +0000

    brkie

---
+++ hosts/acdc/configuration.nix
@@ -0,0 +1,154 @@
+{
+  config,
+    pkgs,
+    lib,
+    inputs,
+    ...
+}:
+
+{
+  imports = [
+    ./harware.nix
+  ];
+
+  config = {
+    nixpkgs.hostPlatform.system = "x86_64-linux";
+    system.stateVersion = "23.05";
+
+### NETWORKING ###
+    networking.hostName = "ruxy-nixos"; # Define your hostname.
+      networking.useDHCP = false;
+    networking.bridges = {
+      "br0" = {
+        interfaces = [ "enp7s0" ];
+      };
+    };
+    networking.interfaces.br0.ipv4.addresses = [ {
+      address = "192.168.1.69";
+      prefixLength = 24;
+    } ];
+    networking.defaultGateway = "192.168.1.1";
+    networking.nameservers = ["192.168.1.1" "8.8.8.8"];
+######
+
+    networking.firewall.enable = false;
+    networking.enableIPv6 = false;
+# Make sure opengl is enabled
+# Make sure opengl is enabled
+    hardware = {
+      opengl.extraPackages= with pkgs; [
+        vaapiVdpau
+          libvdpau-va-gl
+          amdvlk
+      ];
+    };
+    virtualisation.docker.rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+    users.users.ruxy = {
+      isNormalUser = true;
+      extraGroups = [ "docker" "wheel" "libvirtd" "kvm" "input" "disk" "libvirtd" "video" "audio"]; # Enable ‘sudo’ for the user.
+        packages = with pkgs; [
+        lutris
+          steam
+          xfce.thunar
+          cura
+          firefox
+          arduino
+          discord
+          tree
+          obs-studio
+        ];
+    };
+    services.nix-serve = {
+      enable = true;
+      secretKeyFile = "/home/ruxy/keys/cache-priv-key.pem";
+    };
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+    environment.systemPackages = with pkgs; [
+# Virt stuff
+      virt-manager
+        virt-viewer
+        win-virtio
+        OVMF
+        qemu
+        qemu_kvm
+        spice
+#####
+        write_stylus
+        libimobiledevice
+        freecad
+        (lutris.override {
+         extraPkgs = pkgs: [
+         wineWowPackages.stable
+         winetricks
+         ];
+         })
+
+    ];
+    systemd.tmpfiles.rules = [
+      "f /dev/shm/looking-glass 0660 ruxy qemu-libvirtd -"
+    ];
+
+# more virt
+    virtualisation.libvirtd = {
+      enable = true;
+    };
+# CHANGE: add your own user here
+    users.groups.libvirtd.members = [ "root" "ruxy"];
+    virtualisation.libvirtd.qemu.verbatimConfig = ''
+      nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
+      '';
+    services.udev.extraRules = ''
+# UDEV rules for Teensy USB devices
+      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+      KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+# quest
+      SUBSYSTEM="usb", ATTR{idVendor}=="2833", ATTR{idProduct}=="0186", MODE="0660" group="plugdev", symlink+="ocuquest%n"
+
+      '';
+    services = {
+      syncthing = {
+        enable = true;
+        user = "ruxy";
+        dataDir = "/home/ruxy/syncthing";
+        configDir = "/home/ruxy/syncthing/.config/syncthing";
+        overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+          overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+          devices = {
+            "touch" = { id = "UTCVJC7-GLJRFVT-HP2AW2J-KSVJVUU-F5JVJG3-LKR2CSJ-BF5K45V-O7Z3AAP"; };
+            "boox" = { id =  "YQYN5TV-K7IPUBA-LYOMHSB-HR2R7IF-C4PUAKP-Q237CBU-D7ZJKZO-N4MLDQZ"; };
+          };
+        folders = {
+          "Music" = {        # Name of folder in Syncthing, also the folder ID
+            path = "/home/ruxy/Music";    # Which folder to add to Syncthing
+              devices = [ "touch" "boox" ];      # Which devices to share the folder with
+          };
+          "Books" = {        # Name of folder in Syncthing, also the folder ID
+            path = "/home/ruxy/Books";    # Which folder to add to Syncthing
+              devices = [ "touch" "boox" ];      # Which devices to share the folder with
+          };
+        };
+      };
+    };
+    services.openssh = {
+      enable = true;
+# require public key authentication for better security
+      settings.PasswordAuthentication = true;
+      settings.KbdInteractiveAuthentication = false;
+#settings.PermitRootLogin = "yes";
+    };
+  };
+}
+
---
+++ shell/dev.nix
@@ -0,0 +1,156 @@
+{ pkgs, inputs, ... }:
+let
+  minimalMkShell = import ./_minimal.nix { inherit pkgs; };
+
+  # 16 is broken: https://github.com/NixOS/nixpkgs/issues/244609
+  # llvmPackages = pkgs.llvmPackages_16;
+  llvmPackages = pkgs.llvmPackages_15;
+
+  gstreamerPath =
+    ""
+    + ":"
+    + "${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0"
+    + ":"
+    + "${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
+    + ":"
+    + "${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0"
+    + ":"
+    + "${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0";
+
+  _rustBuildFenix = (
+    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.withComponents [
+      "cargo"
+      "clippy"
+      "rust-src"
+      "rustc"
+      "rustfmt"
+      "rust-analyzer"
+    ]
+  );
+
+  _rustBuildOxalica = inputs.rust-overlay.packages.${pkgs.stdenv.hostPlatform.system}.rust.override {
+    extensions = [
+      "rust-src"
+      "rust-analyzer"
+      "clippy"
+    ];
+  };
+
+  # so far I can't tell a big difference...
+  _rustBuild = _rustBuildOxalica;
+in
+# _rustBuild = _rustBuildFenix;
+minimalMkShell {
+  name = "cole-nixcfg-dev";
+  hardeningDisable = [ "fortify" ];
+
+  shellHook = ''
+    exec nu
+  '';
+
+  LD_LIBRARY_PATH = "${pkgs.libglvnd}/lib";
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  RUST_BACKTRACE = 1;
+  GST_PLUGIN_SYSTEM_PATH = gstreamerPath;
+
+  nativeBuildInputs = with pkgs; [
+    _rustBuild
+    llvmPackages.lldb
+
+    inputs.nix-eval-jobs.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default
+
+    nix
+
+    ## nix lsp
+    rnix-lsp
+    nil
+
+    ## nix space usage / visualizers
+    nix-du
+    nix-tree
+
+    # nix formatters
+    nixpkgs-fmt
+    nixfmt
+    alejandra
+
+    ## nodejs
+    nodejs
+    yarn
+
+    ## golang
+    go
+    go-outline
+    gotools
+    godef
+    gopls
+
+    # generic build essentials
+    pkg-config
+    cmake
+    gnumake
+    nasm
+    perl
+
+    # json tools
+    gron
+
+    gst_all_1.gstreamer
+
+    protobuf
+
+    # not a good sign
+    dos2unix
+  ];
+
+  buildInputs = with pkgs; [
+    llvmPackages.libclang
+    llvmPackages.libclang.lib
+    pipewire
+    freetype
+    ncurses
+    pcsclite
+    openssl
+    clang
+    libusb1
+    gpgme
+    libgpg-error
+    libgit2
+    git # passrs
+    dbus # passrs libsecret
+    nettle # pass-rust (sequoia->nettle-sys)
+    gst_all_1.gstreamer
+    libnice
+    pango
+    cairo
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+
+    crate2nix
+
+    glslang
+    gtk3
+    gtk4
+
+    atk # sirula
+    gdk-pixbuf # sirula
+    udev
+    mesa
+    libinput # Anodium
+    seatd # Anodium
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi # Anodium
+    libxkbcommon
+    wayland
+    wayland-protocols # wezterm
+    fontconfig
+    libglvnd
+    opencv
+    ffmpeg
+    egl-wayland # wezterm
+  ];
+}
